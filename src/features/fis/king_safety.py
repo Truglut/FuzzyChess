a04c8dff8b69c03fe6acc4a499f105d9d30dca75
Truglut <contrerasdesantos.andres@gmail.com>
2026-04-23 20:38:20 +0200

    make king safety fis color independent

--- src/features/fis/king_safety.py
+++ src/features/fis/king_safety.py
@@ -6,15 +6,13 @@ KING_SAFETY_LUT_PATH = "data/luts/safety_lut.npy"
 MAX_KING_ATTACKERS_DIFF = 4
 MIN_KING_ATTACKERS_DIFF = -4
 
+
 class KingSafetyFIS(FIS):
-    def __init__(self, lut_path: str, color: chess.Color = chess.WHITE):
+    def __init__(self, lut_path: str):
         super().__init__(lut_path)
-        self.color = color
 
-
-    def extract_params(self, board: chess.Board) -> tuple:
-        return get_king_safety_params(board, self.color)
-    
+    def extract_params(self, board: chess.Board, color: chess.Color) -> tuple:
+        return get_king_safety_params(board, color)
 
     def lookup(self, params: tuple) -> float:
         att_diff, shield = params
@@ -22,4 +20,9 @@ class KingSafetyFIS(FIS):
         att_diff = max(min(att_diff, MAX_KING_ATTACKERS_DIFF), MIN_KING_ATTACKERS_DIFF)
 
         return self.lut[att_diff + 4, shield]
-    
+
+    def compute(self, board: chess.Board, color: chess.Color):
+        return self.lookup(self.extract_params(board, color))
+
+    def __call__(self, board: chess.Board, color: chess.Color):
+        return self.compute(board, color)
