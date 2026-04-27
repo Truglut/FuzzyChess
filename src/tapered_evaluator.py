b73bb733acb203d20514573f77f9a96ffaaceae6
Truglut <contrerasdesantos.andres@gmail.com>
2026-04-27 20:51:55 +0200

    feat: add TaperedEvaluator class

---
+++ src/tapered_evaluator.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+import torch
+
+
+class TaperedEvaluator(nn.Module):
+    def __init__(self, mg_evaluator: nn.Module, eg_evaluator: nn.Module):
+        """
+        Orchestrates tapered evaluation using a middlegame evaluator and
+        and endgame evaluator.
+        """
+        super().__init__()
+
+        self.mg_evaluator = mg_evaluator
+        self.eg_evaluator = eg_evaluator
+
+    def forward(
+        self, x_mg: torch.Tensor, x_eg: torch.Tensor, phase: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Calculates the final tapered eval.
+        """
+
+        mg_score = self.mg_evaluator(x_mg)
+        eg_score = self.eg_evaluator(x_eg)
+
+        tapered_score = phase * mg_score + (1.0 - phase) * eg_score
+        return tapered_score
